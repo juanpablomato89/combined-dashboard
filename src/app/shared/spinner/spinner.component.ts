f4d5839aee3c14b74d7582f4a1e812a45400c2b0
Juan Pablo Mato Hernandez <juanpablomato89@gmail.com>
2025-11-20 23:02:47 -0500

    configurando el spinner

--- src/app/shared/spinner/spinner.component.ts
+++ src/app/shared/spinner/spinner.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-spinner',
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./spinner.component.css']
 })
 export class SpinnerComponent {
+  @Input() visible: boolean = false;
+  @Input() text: string = '';
 
 }
